a12cb775646d2c655f272f66b9ff5bc670e8566a
thangtran238 <bondshino.238@gmail.com>
2024-06-04 14:13:09 +0700

    update staging branch

--- webapp/api/authApi.ts
+++
@@ -1,7 +0,0 @@
-import { facilities } from "./config";
-
-export const authApi = {
-	login: async (username: string, password: string) => {
-		return await facilities.post("public/login", { username, password });
-	},
-};
