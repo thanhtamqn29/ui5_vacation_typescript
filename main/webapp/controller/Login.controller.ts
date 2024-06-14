9ffe16d4fccbb87dfe7a284c5047d04f8150783c
thangtran238 <bondshino.238@gmail.com>
2024-06-14 14:10:53 +0700

    remove test authen

--- main/webapp/controller/Login.controller.ts
+++ main/webapp/controller/Login.controller.ts
@@ -37,7 +37,7 @@ export default class Login extends BaseController {
 			localStorage.setItem("accessToken", accessToken);
 			loadingDialog.hide();
 
-			await this.test();
+			await this.navigation();
 		} catch (error) {
 			MessageBox.error(
 				error.response.data?.error?.message ||
@@ -49,45 +49,58 @@ export default class Login extends BaseController {
 		}
 	}
 
-	private async test() {
-		try {
-			const userVerified = await axios.post(
-				"https://ap5x0pdek.trial-accounts.ondemand.com/oauth2/token",
-				`grant_type=password&client_id=af8dbd38-ee9d-4d94-a167-c9641075211f&client_secret=XEZ5ZCyXlz6=t18AkyupOZcnHydrCd&username=bondshino238@gmail.com&password=Shino238@`,
-				{
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-				}
-			);
-			console.log(userVerified);
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	// private async navigation() {
+	// private async test() {
 	// 	try {
-	// 		loadingDialog.show();
-	// 		const { data } = await authApi.getCurrentUser(
-	// 			localStorage.getItem("accessToken")
-	// 		);
-	// 		const getInfo = data.value[0];
-
-	// 		if (getInfo.department_id) this.navTo("hrManager");
-	// 		else {
-	// 			if (getInfo.role === "staff") this.navTo("main");
-	// 			if (getInfo.role === "manager") this.navTo("manager");
-	// 		}
-	// 		loadingDialog.hide();
-	// 	} catch (error) {
-	// 		MessageBox.error(
-	// 			error.response.data?.error?.message ||
-	// 				"An error occurred while creating the leave request.",
+	// 		const {data} = await axios.post(
+	// 			"https://ap5x0pdek.trial-accounts.ondemand.com/oauth2/token",
+	// 			`grant_type=password&client_id=af8dbd38-ee9d-4d94-a167-c9641075211f&client_secret=XEZ5ZCyXlz6=t18AkyupOZcnHydrCd&username=bondshino238@gmail.com&password=Shino238@`,
 	// 			{
-	// 				onClose: () => loadingDialog.hide(),
+	// 				headers: {
+	// 					"Content-Type": "application/x-www-form-urlencoded",
+	// 				},
 	// 			}
 	// 		);
+	// 		console.log(data);
+
+	// 		const accessToken = data.access_token;
+	// 		console.log(accessToken);
+			
+	// 		const getUser = await axios.get(
+	// 			"https://ap5x0pdek.trial-accounts.ondemand.com/oauth2/userinfo", {
+	// 				headers: {
+	// 					Authorization: `Bearer ${accessToken}`,
+	// 				}
+	// 			}
+	// 		)
+	// 		console.log(getUser);
+			
+	// 	} catch (error) {
+	// 		console.log(error);
 	// 	}
 	// }
+
+	private async navigation() {
+		try {
+			loadingDialog.show();
+			const { data } = await authApi.getCurrentUser(
+				localStorage.getItem("accessToken")
+			);
+			const getInfo = data.value[0];
+
+			if (getInfo.department_id) this.navTo("hrManager");
+			else {
+				if (getInfo.role === "staff") this.navTo("main");
+				if (getInfo.role === "manager") this.navTo("manager");
+			}
+			loadingDialog.hide();
+		} catch (error) {
+			MessageBox.error(
+				error.response.data?.error?.message ||
+					"An error occurred while creating the leave request.",
+				{
+					onClose: () => loadingDialog.hide(),
+				}
+			);
+		}
+	}
 }
