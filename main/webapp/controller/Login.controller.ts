ce7bdb162d40e8ce4a043fa34d551f88dfc1e576
thangtran238 <bondshino.238@gmail.com>
2024-06-14 09:10:48 +0700

    test oauth2/token

--- main/webapp/controller/Login.controller.ts
+++ main/webapp/controller/Login.controller.ts
@@ -3,12 +3,13 @@ import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import MessageBox from "sap/m/MessageBox";
 import { isLoading } from "myapp/utils/busyIndicator";
+import axios from "axios";
 
 const loadingDialog = new isLoading();
 export default class Login extends BaseController {
 	public async onInit(): Promise<void> {
 		const oModel = new JSONModel();
-
+		
 		await oModel.loadData("/user-api/currentUser");
 
 		this.getView().setModel(oModel);
@@ -36,7 +37,7 @@ export default class Login extends BaseController {
 			localStorage.setItem("accessToken", accessToken);
 			loadingDialog.hide();
 
-			await this.navigation();
+			await this.test();
 		} catch (error) {
 			MessageBox.error(
 				error.response.data?.error?.message ||
@@ -48,28 +49,45 @@ export default class Login extends BaseController {
 		}
 	}
 
-	private async navigation() {
+	private async test() {
 		try {
-			loadingDialog.show();
-			const { data } = await authApi.getCurrentUser(
-				localStorage.getItem("accessToken")
-			);
-			const getInfo = data.value[0];
-
-			if (getInfo.department_id) this.navTo("hrManager");
-			else {
-				if (getInfo.role === "staff") this.navTo("main");
-				if (getInfo.role === "manager") this.navTo("manager");
-			}
-			loadingDialog.hide();
-		} catch (error) {
-			MessageBox.error(
-				error.response.data?.error?.message ||
-					"An error occurred while creating the leave request.",
+			const userVerified = await axios.post(
+				"https://ap5x0pdek.trial-accounts.ondemand.com/oauth2/token",
+				`grant_type=password&client_id=af8dbd38-ee9d-4d94-a167-c9641075211f&client_secret=XEZ5ZCyXlz6=t18AkyupOZcnHydrCd&username=bondshino238@gmail.com&password=Shino238@`,
 				{
-					onClose: () => loadingDialog.hide(),
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded",
+					},
 				}
 			);
+			console.log(userVerified);
+		} catch (error) {
+			console.log(error);
 		}
 	}
+
+	// private async navigation() {
+	// 	try {
+	// 		loadingDialog.show();
+	// 		const { data } = await authApi.getCurrentUser(
+	// 			localStorage.getItem("accessToken")
+	// 		);
+	// 		const getInfo = data.value[0];
+
+	// 		if (getInfo.department_id) this.navTo("hrManager");
+	// 		else {
+	// 			if (getInfo.role === "staff") this.navTo("main");
+	// 			if (getInfo.role === "manager") this.navTo("manager");
+	// 		}
+	// 		loadingDialog.hide();
+	// 	} catch (error) {
+	// 		MessageBox.error(
+	// 			error.response.data?.error?.message ||
+	// 				"An error occurred while creating the leave request.",
+	// 			{
+	// 				onClose: () => loadingDialog.hide(),
+	// 			}
+	// 		);
+	// 	}
+	// }
 }
